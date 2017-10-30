5d1a393c205552e06aa7cbf8bf68f83e792fd311
Helen Shiu <helen@quilt9.com>
2017-10-30 19:12:13 +0000

    chore: prep both html and js with comments about the project objectives; no production code change

--- designs.js
+++ designs.js
@@ -1,4 +1,16 @@
+/**
+APP: Pixel Art Maker
+User Stories:
+1. Dynamically set the size of the table as an N by M grid.
+2. Choose a color.
+3. Click a cell in the grid to fill that cell with the chosen color
+===================================================================
+Project Objectives
+1. Implement the makeGrid() function that dynamically creates a grid that the user can interact with.
+**/
+
 // Select color input
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
