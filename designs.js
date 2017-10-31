03649a783c8a88a31458e1185d5edd6370ae3885
Helen Shiu <helen@quilt9.com>
2017-10-31 18:52:16 +0000

    feat: add the selector to capture the inputs when submit clicked

--- designs.js
+++ designs.js
@@ -1,4 +1,6 @@
 /* global $ */
+/* global let */
+/* global html */
 /**
 APP: Pixel Art Maker
 User Stories:
@@ -6,8 +8,13 @@ User Stories:
 2. Choose a color.
 3. Click a cell in the grid to fill that cell with the chosen color
 ===================================================================
-Project Objectives
-1. Implement the makeGrid() function that dynamically creates a grid that the user can interact with.
+Project Objective
+Implement the makeGrid() function that dynamically creates a grid that the user can interact with.
+===================================================================
+Project Tasks
+1. Capture the value of the inputs for both number of columns and rows.
+2. Listen for keypress event.
+3. Insert the number of columns and rows based on the value of the inputs.
 **/
 
 $(document).ready(function() {
@@ -15,16 +22,25 @@ $(document).ready(function() {
     // Select color input
 
     // Select size input
-    
+    // The event handler bound to #sizePicker
+    $(':submit').on('click', function(e){
+        let heightNum = $('#input_height').val();
+        let widthNum = $('#input_width').val();
+        makeGrid(heightNum, widthNum);
+    });
 
     // When size is submitted by the user, call makeGrid()
-    
-    function makeGrid() {
-    
     // Your code goes here!
-    
+    function makeGrid(heightNum, widthNum) {
+        let rowNum = parseInt(heightNum);
+        let colNum = parseInt(widthNum);
+        // Create rows based on numRow
+        for(let i = 1; i <= rowNum; i++) {
+             // What needs to happen to create the table columns and rows
+            $('#pixel_canvas').append('<tr><td>i+1</td></tr>');
+        }
     }
-    
+   
 });
 
 
